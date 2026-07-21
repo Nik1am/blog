122f802648f958e48411737c5193d91fd219a2bc
Nikita Parus <nikiam1337@gmail.com>
2026-07-21 21:03:00 +0200

    routing test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-// import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
+import posts from "./postIndex.json";
 function App() {
+  const [content, setContent] = useState("");
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        <div id="sidebar">
+          <ul>
+            {posts.map((e) => (
+              <li>
+                <button
+                  onClick={() => {
+                    fetch(`/posts/${e.file}.md`)
+                      .then((res) => res.text())
+                      .then((text) => setContent(text));
+                  }}
+                >
+                  {e.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <main>
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
